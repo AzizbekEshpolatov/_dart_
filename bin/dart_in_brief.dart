1420565c00e7bb007c1b11ff7dc16765554314c6
Flutter <aziz2311e@gmail.com>
2025-01-05 17:18:59 +0500

    dart_overview

---
+++ bin/dart_in_brief.dart
@@ -0,0 +1,16 @@
+// ----- Dart dasturlash tili -----
+
+/// Dart dasturlash tili Google tomonidan ishlab chiqilgan yuqori samaradorlikka
+/// ega, obyektga yonaltirilgan, ochiq manba kodli dasturlash tili. U asosan
+/// Flutter frameworki yordamida mobil/web/deskop dasturlar ishlab chiqishga
+/// moslashgan.
+/// Dart dasturlash tilining afvzalliklari:
+/// * Null xavfsizligi (Sound Null Safety): Dartda har bir o‘zgaruvchi qiymat
+/// null bo‘lishi mumkinmi yoki yo‘qmi aniq belgilanadi.
+/// * Asinxron dasturlash uchun qulay vositalar:Vaqt talab qiladigan
+/// operatsiyalarni (masalan, tarmoq so‘rovlari) boshqarishni oddiy va
+/// tushunarli qiladi.
+/// * standart kutubxonalar, Dart o‘z ichida quyidagi imkoniyatlarni taqdim
+/// etadigan boy kutubxonalarga ega. dart:math — matematik funksiyalar va
+/// tasodifiy sonlar. dart:convert — JSON va boshqa formatlar uchun kodlash va
+/// dekodlash. dart:io — fayllar va tarmoqlar bilan ishlash.
