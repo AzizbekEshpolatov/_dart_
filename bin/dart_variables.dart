8cb5b60d9228c839d3cb56e713f70eeb431fde60
Flutter <aziz2311e@gmail.com>
2025-01-06 13:41:43 +0500

    dart_veriable

---
+++ bin/dart_variables.dart
@@ -0,0 +1,16 @@
+/// Dart tilida o‘zgaruvchilarni (variables) yaratish va ulardan foydalanish
+/// quyidagicha amalga oshiriladi:
+
+/// * var yordamida o‘zgaruvchi e’lon qilinsa, Dart o‘zgaruvchining turini
+/// avtomatik aniqlab oladi.
+// var name = 'Ali';
+// var age = 25;
+
+/// * Agar o‘zgaruvchining turini aniq ko‘rsatmoqchi bo‘lsangiz, quyidagicha
+/// qilasiz:
+// String shahar = 'Toshkent';
+// int yosh = 30;
+
+/// * final va const bilan o‘zgarmas o‘zgaruvchilar:
+/// Agar o‘zgaruvchi qiymati keyinchalik o‘zgarmasligini istasangiz, final yoki
+/// constdan foydalaning.
